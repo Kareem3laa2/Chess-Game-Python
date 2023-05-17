e971cb29b4afac4980312df565f64a3a575056ab
hazemr2121 <ha.ahmed8996@gmail.com>
2023-05-17 18:40:38 +0300

    Console Board + Creating Pieces

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 import pygame
-
+from board import  Board
 from const import *
 
 
 
 class Game:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.board = Board()
 
     #show methods
 
@@ -20,3 +20,17 @@ class Game:
                 rect = (col * SQIZE, row * SQIZE, SQIZE,SQIZE)
 
                 pygame.draw.rect(surface,color,rect)
+
+
+    def show_pieces(self,surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                # piece ?
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQIZE + SQIZE // 2 , row * SQIZE + SQIZE // 2
+                    piece.texture_rect = img.get_rect(center = img_center)
+                    surface.blit(img,piece.texture_rect)
+                    
